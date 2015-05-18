9cd4d456d954edfcd9fc1d88639463db22a885db
Denisson Silva <denisson@denissonsilva.com>
2015-05-18 10:08:14 -0300

    Theme_CPP

---
+++ theme_cpp.R
@@ -0,0 +1,20 @@
+# Theme_CPP 
+# Tema para ggplot2
+# Denisson Silva / denisson@denissonsilva.com
+# Willber Nascimento / willbernascimento@outlook.com
+
+
+theme_blank<- function (base_size = 12, base_family = "") {
+  theme_gray(base_size = base_size, base_family = base_family) %+replace% 
+    theme(
+      axis.text = element_text(colour = "black"),
+      axis.title.x = element_text(colour = "black", size=rel(3)),
+      axis.title.y = element_text(colour = "black", angle=45),
+      panel.grid = element_blank(),
+      panel.background = element_rect(fill="white"),
+      plot.background = element_rect(fill="white")
+    )   
+}
+
+theme_set(theme_blank())
+theme_blank <- theme_get()
